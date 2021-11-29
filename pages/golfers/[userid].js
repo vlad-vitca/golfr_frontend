3be1c7dd47b9da84dd5f72f3eda42ca01a8412f8
Vlad Ilie Vitca <vlad.vitca@golfgenius.com>
2021-11-29 13:45:24 +0200

    Removed useless fetch

--- pages/golfers/[userid].js
+++ pages/golfers/[userid].js
@@ -1,9 +1,8 @@
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
 import Layout from '../../components/Layout'
 import ScoreCard from '../../components/ScoreCard'
 import useUserScores from '../../lib/useUserScores'
-import { USER_NAME_URL, USER_SCORES_URL } from '../../lib/useUserScores'
+import { USER_SCORES_URL } from '../../lib/useUserScores'
 
 const GolferScores = () => {
   const router = useRouter()
@@ -11,30 +10,30 @@ const GolferScores = () => {
   let { name, scores, error } = useUserScores(userid)
 
   if (error) {
-    return <Layout> { error.info.errors } </Layout>
+    return <Layout> {error.info.errors} </Layout>
   }
 
   return (
-  <Layout>
-    {
-      name && (
-        <>
-          <h1> The scores of user {name}: </h1>
-          {scores && scores.map(score => (
-            <ScoreCard
-              key={score.id}
-              id={score.id}
-              totalScore={score.total_score}
-              playedAt={score.played_at}
-              userId={score.user_id}
-              userName={name}
-              fetchUrls={[USER_NAME_URL(userid), USER_SCORES_URL(userid)]}
-            />
-          ))}
-        </>
-      )
-    }
-  </Layout>
+    <Layout>
+      {
+        name && (
+          <>
+            <h1> The scores of user {name}: </h1>
+            {scores && scores.map(score => (
+              <ScoreCard
+                key={score.id}
+                id={score.id}
+                totalScore={score.total_score}
+                playedAt={score.played_at}
+                userId={score.user_id}
+                userName={name}
+                fetchUrls={[USER_SCORES_URL(userid)]}
+              />
+            ))}
+          </>
+        )
+      }
+    </Layout>
   )
 }
 
